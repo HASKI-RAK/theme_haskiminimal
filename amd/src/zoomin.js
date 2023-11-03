678d2b3a017b1807827d7efaf84fb003c066ce77
Kevin Lee <kevinkimlee95@gmail.com>
2023-11-03 15:11:41 +0000

    Added +, - Buttons to H5PContent

---
+++ amd/src/zoomin.js
@@ -0,0 +1,4 @@
+const zoom_in = () => {
+    h5pelement = document.getElementById('h5p-iframe-wrapper');
+    
+};
--- lib.php
+++ lib.php
@@ -23,7 +23,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-// We will add callbacks here as we add features to our theme.
 function theme_haskiminimal_get_main_scss_content($theme) {
     global $CFG;
 
@@ -33,24 +32,17 @@ function theme_haskiminimal_get_main_scss_content($theme) {
 
     $context = context_system::instance();
     if ($filename == 'default.scss') {
-        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
     } else if ($filename == 'plain.scss') {
-        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
 
     } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_haskiminimal', 'preset', 0, '/', $filename))) {
-        // This preset file was fetched from the file area for theme_haskiminimal and not theme_boost (see the line above).
         $scss .= $presetfile->get_content();
     } else {
-        // Safety fallback - maybe new installs etc.
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
     }
-    // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
     $pre = file_get_contents($CFG->dirroot . '/theme/haskiminimal/scss/pre.scss');
-    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
     $post = file_get_contents($CFG->dirroot . '/theme/haskiminimal/scss/post.scss');
-    // Combine them together.
     return $pre . "\n" . $scss . "\n" . $post;
 }
 
