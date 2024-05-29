399659d4756d831e4c7e456a2e12a37bc46f30af
Kevin Lee <kevinkimlee95@gmail.com>
2024-05-29 09:37:37 +0000

    fallback removal of navigation elements on every page

--- amd/src/removecontinue.js
+++ amd/src/removecontinue.js
@@ -6,36 +6,34 @@ export const init = () => {
                 button.style.visibility = "hidden";
             });
         }
-        if (window.location.href.includes('feedback')){
-            let element = document.getElementById("page-header");
-            if(element !== null && element !== undefined){
-                element.style.display = 'none';
-                element.style.height = '0px';
-            }
-            element = document.getElementById('theme_boost-drawers-courseindex');
-            if(element !== null && element !== undefined){
-              element.style.display = 'none';
-            }
-            element = document.querySelector('.navbar.fixed-top.navbar-light.bg-white');
-            if(element !== null  && element !== undefined){
-                element.style.visibility = 'hidden';
-                element.style.height = '0px';
-            }
-            element = document.querySelector('#page.drawers.show-drawer-left.drag-container');
-            if(element !== null  && element !== undefined){
-                        element.style.marginLeft = '0px';
-            element.style.marginTop = '0px';
-            element.style.height = '100%';
-            }
-            element = document.getElementsByClassName('activity-navigation')[0];
-            if(element !== null  && element !== undefined){
-                element.style.visibility = 'hidden';
-            }
-            element = document.querySelector('.nav.more-nav.nav-tabs');
-            if(element !== null  && element !== undefined){
-                element.style.visibility = 'hidden';
-                element.style.height = '0px';
-            }
+        let element = document.getElementById("page-header");
+        if(element !== null && element !== undefined){
+            element.style.display = 'none';
+            element.style.height = '0px';
+        }
+        element = document.getElementById('theme_boost-drawers-courseindex');
+        if(element !== null && element !== undefined){
+          element.style.display = 'none';
+        }
+        element = document.querySelector('.navbar.fixed-top.navbar-light.bg-white');
+        if(element !== null  && element !== undefined){
+            element.style.visibility = 'hidden';
+            element.style.height = '0px';
+        }
+        element = document.querySelector('#page.drawers.show-drawer-left.drag-container');
+        if(element !== null  && element !== undefined){
+                    element.style.marginLeft = '0px';
+        element.style.marginTop = '0px';
+        element.style.height = '100%';
+        }
+        element = document.getElementsByClassName('activity-navigation')[0];
+        if(element !== null  && element !== undefined){
+            element.style.visibility = 'hidden';
+        }
+        element = document.querySelector('.nav.more-nav.nav-tabs');
+        if(element !== null  && element !== undefined){
+            element.style.visibility = 'hidden';
+            element.style.height = '0px';
         }
     }
 };
